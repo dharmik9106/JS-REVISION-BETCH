90ece936caf5605652019108755316d68b6bfed7
Dharmik Ahir <sangadharmik78@gmail.com>
2025-12-17 11:21:18 +0530

    Prectice  Quetion

---
+++ index (2).js
@@ -0,0 +1,60 @@
+// 1
+console.log("5" + 5);  // 55
+
+// 2
+console.log("5" - "2"); //3
+
+// 3
+console.log("4" * "3");//12
+
+// 4
+console.log("20" / 4);//5
+
+// 5
+console.log(true + true);//2
+
+// 6
+console.log(true - false);//1
+
+// 7
+console.log(null + null);//0
+
+// 8
+console.log(undefined + null);//nan
+
+// 9
+console.log("false" + true);//falsetrue
+
+// 10
+console.log("15" - true);//14
+
+// 11
+console.log(Number(true));//1
+
+// 12
+console.log(Number(false));//0
+
+// 13
+console.log(Number(" "));//0
+
+// 14
+console.log(parseInt("123abc45"));//123
+
+// 15
+console.log(parseFloat("12.34.56"));//12.34
+
+// 16
+console.log(String(null));//null
+
+// 17
+console.log(Boolean(0));//false
+
+// 18
+console.log(Boolean("0"));//true
+
+// 19
+console.log(Boolean(undefined));//false
+
+// 20
+let x = "10" * 2;
+console.log(x, typeof x);//20 number
